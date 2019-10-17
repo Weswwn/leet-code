caa41dc8b7da91b2d79ffa3830f25a9a656a478c
Wes <wesley.qb.wen@gmail.com>
2019-10-16 20:16:53 -0700

    First round completion with 5% faster than others

---
+++ longestSubString.js
@@ -0,0 +1,39 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    var longestSubString = 1;
+    var count = 1;
+    var charChecker = 0;
+    var objHolder = {};
+    objHolder[s[charChecker]] = s[charChecker];
+
+    if (s.length === 0) {
+        return 0;
+    } else if (s.length === 1) {
+        return longestSubString;
+    }
+
+    for (var i = 0; i < s.length; i++) {
+        if (i + 1 === s.length) {
+            return longestSubString;
+        }
+        if (s[charChecker] !== s[i + 1] && !objHolder.hasOwnProperty(s[i + 1])) {
+            objHolder[s[i+1]] = s[i+1];
+            count++;
+            if (count > longestSubString) {
+                longestSubString = count;
+            }
+        } else {
+            count = 1;
+            charChecker++;
+            objHolder = {};
+            objHolder[s[charChecker]] = s[charChecker];
+            i = charChecker - 1;
+        }
+    }
+    return longestSubString;
+};
+
+console.log(lengthOfLongestSubstring("bbbbb"));
