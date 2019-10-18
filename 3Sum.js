5edc9524634faf4fedd618aa905345662cef0112
Wes <wesley.qb.wen@gmail.com>
2019-10-17 20:38:06 -0700

    First round completion with 25% faster than others

--- 3Sum.js
+++ 3Sum.js
@@ -3,26 +3,38 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    var resultArray  = [];
-    var objTracker = {};
-    for (var i = 0; i < nums.length; i++) {
-        for (var z = 1; z < nums.length; z++) {
-            if (z !== i) {
-                for (var y = 2; y < nums.length; y++) {
-                    var tempArray = [];
-                    if (y !== i && y !== z) {
-                        if (nums[i] + nums[z] + nums[y] === 0) {
-                            tempArray.push(nums[i], nums[y], nums[z]);
-                            tempArray.sort();
-                            if (!objTracker[tempArray]) {
-                                objTracker[tempArray] = tempArray;
-                                resultArray.push(tempArray);
-                            }
-                        }
-                    }
+    nums.sort((a,b) => {
+        return a - b;
+    })
+    var obj = {};
+    var resultArray = [];
+    
+    for (let i = 0; i < nums.length; i++) {
+        let L = i + 1;
+        let R = nums.length - 1;
+        
+        if (i > 0 && nums[i] == nums[i - 1]) {
+			continue;
+		}
+        
+        while(L < R) {
+            if (nums[i] + nums[L] + nums[R] === 0) {
+                var temp = [nums[i] , nums[L], nums[R]];
+                temp.sort();
+                if (!obj[temp]) {
+                    obj[temp] = temp;
+                    resultArray.push(temp);
                 }
+                L++;
+                R--;
+            }
+            if (nums[i] + nums[L] + nums[R] > 0) {
+                R--;
+            }
+            if (nums[i] + nums[L] + nums[R] < 0) {
+                L++;
             }
         }
     }
     return resultArray;
-};
+}; 
