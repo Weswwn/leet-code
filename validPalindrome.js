cc1a7048799848a98ba619aa6d5a5ff6d1ce06bb
Wes <wesley.qb.wen@gmail.com>
2019-10-31 01:19:22 -0700

    Added second implementation style at bottom

--- validPalindrome.js
+++ validPalindrome.js
@@ -17,3 +17,18 @@ var isPalindrome = function(x) {
 
 
 // https://dpaste.de/369b
+
+/*
+function reverse(num) {
+    let ret = 0;
+    while (num) {
+        ret = 10 * ret + num % 10;
+        num = Math.floor(num / 10);
+    }
+    return ret;
+}
+
+function isPalindrome(x) {
+    return x >= 0 && x === reverse(x);
+}
+*/
