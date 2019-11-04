bf8da95993f86a9e9e9d774a59134be765aad999
Wes <wesley.qb.wen@gmail.com>
2019-11-03 17:30:19 -0800

    Second implementation with Breadth First Search

--- traverseGraph.js
+++ traverseGraph.js
@@ -1,13 +1,23 @@
 let traverseGraph = (root) => {
     let allValues = [];
+    let queue = [];
     if (!root) return allValues;
     let recurse = (node) => {
-        if (!node) return allValues;
-        if (node.isVisited === true) return;
-        allValues.push(node.val);
-        node.isVisited = true;
-        for (let i = 0; i < node.children.length; i++) {
-            recurse(node.children[i]);
+        if (!node) return;
+        if (node.isVisited === true) {
+            let poppedNode = queue.shift();
+            recurse(poppedNode);
+        } else {
+            allValues.push(node.val);
+            node.isVisited = true;
+            for (let i = 0; i < node.children.length; i++) {
+                queue.push(node.children[i]);
+            }
+            if (queue.length > 0) {
+                let poppedNode = queue.shift();
+                console.log(poppedNode);
+                recurse(poppedNode);
+            }
         }
     }
     recurse(root);
@@ -20,11 +30,16 @@ let GraphNode = function(val) {
     this.children = [];
 }
 
-let root = new GraphNode(1);
-let node1 = new GraphNode(2);
-let node2 = new GraphNode(3);
-root.children = [node1];
-node1.children = [node1, node2];
-node2.children = [node1];
 
-console.log(findRoute(root, node1, node2));
+let root = new GraphNode(0);
+let node1 = new GraphNode(1);
+let node2 = new GraphNode(2);
+let node3 = new GraphNode(3);
+let node4 = new GraphNode(4);
+root.children = [node1];
+node1.children = [node2, node4];
+node2.children = [root, node1, node3];
+node3.children = [node1];
+node4.children = [root];
+
+console.log(traverseGraph(root));
