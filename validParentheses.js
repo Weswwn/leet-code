8fbbea4a4911cebcfd9c865773723c404d23af41
Wes <wesley.qb.wen@gmail.com>
2019-11-13 20:54:30 -0800

    Second round completion with 76% faster than others

--- validParentheses.js
+++ validParentheses.js
@@ -3,54 +3,34 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    var stack = [];
-    if (s[0] === ']' || s[0] === '}' || s[0] === ')') {
+    let tracker = [];
+    let stack = [];
+    // let backwardStack = [];
+    if (s.length === 1) {
         return false;
     }
-    if (s.length === 0) {
-        return true;
+    for (let i = 0; i < s.length; i++) {
+        stack.push(s[i]);
+    }  
+    while (stack.length > 0) {
+        let popped = stack.shift();
+        if (popped === '[' || popped === '{' || popped === '(') {
+            tracker.push(popped);
+            continue;
+        }
+        if (popped === '}' && tracker[tracker.length - 1] !== '{') {
+            return false;
+        }
+        if (popped === ']' && tracker[tracker.length - 1] !== '[') {
+            return false;
+        }
+        if (popped === ')' && tracker[tracker.length - 1] !== '(') {
+            return false;
+        }
+        tracker.pop();
     }
-    stack.push(s[0]);
-    
-    for (var i = 1; i < s.length; i++) {
-        if (s[i] === '(') {
-            stack.push(s[i]);
-            continue;
-        } else if (s[i] === '{') {
-            stack.push(s[i]);
-            continue;
-        } else if (s[i] === '[') {
-            stack.push(s[i]);
-            continue;
-        } 
-        if (s[i] === ')') {
-            if (stack[stack.length - 1] === '(') {
-                stack.pop();
-            } else {
-                console.log(stack[stack.length - 1])
-                return false;
-            }
-        }
-        if (s[i] === ']') {
-            if (stack[stack.length - 1] === '[') {
-                stack.pop();
-            } else {
-                // console.log(stack[stack.length - 1])
-                return false;
-            }
-        }
-        if (s[i] === '}') {
-            if (stack[stack.length - 1] === '{') {
-                stack.pop();
-            } else {
-                console.log(stack[stack.length - 1])
-                return false;
-            }
-        }
-    }
-    if (stack.length !== 0) {
+    if (tracker.length > 0) {
         return false;
     }
     return true;
-
 };
