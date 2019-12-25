f222fdb7299d1f5c5eb84c8a1af0b28bffdb33a4
Wes <wesley.qb.wen@gmail.com>
2019-12-24 17:11:00 -0800

    Second round completion with recursion instead of two pointer method

--- reverseString2.0.js
+++ reverseString2.0.js
@@ -1,21 +1,14 @@
-/**
- * @param {character[]} s
- * @return {void} Do not return anything, modify s in-place instead.
- */
 var reverseString = function(s) {
-    if (!s) return;
-    helper(0, s.length - 1, s);
+    const helper = (index) => {
+        let tempIndex = s.length - index - 1;
+        let tempChar = s[tempIndex];
+        if (index > tempIndex) {
+            return s;
+        }
+        s[tempIndex] = s[index];
+        s[index] = tempChar;
+        index++;
+        helper(index);
+    }
+    helper(0);
 };
-
-let helper = (L, R, string) => {
-    if (L >= R) {
-        return;
-    };
-    
-    let temp = string[L];
-    string[L] = string[R];
-    string[R] = temp;
-    helper(L + 1, R - 1, string);
-}
-
-console.log(reverseString('hello'));
