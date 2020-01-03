10160ff4269a11934d4c3a7beb513a977943f2d6
Wes <wesley.qb.wen@gmail.com>
2020-01-02 20:51:51 -0800

    First round completion

---
+++ permutation.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function(nums) {
+  let resultArray = [];
+  
+  var recurse = (array) => {
+      if (array.length === nums.length) {
+          resultArray.push(array);
+          return;
+      }
+      for (var i = 0; i < nums.length; i++) {
+          if (!array.includes(nums[i])) {
+              recurse(array.concat([nums[i]]));
+          }
+      } 
+  }
+  var array = [];
+  recurse(array);
+  return resultArray;
+};
+console.log(permute([1,2,3]));
