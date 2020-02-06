a13c41d3da83381a531bc3d9827107efc87ca5ea
Wes <wesley.qb.wen@gmail.com>
2020-02-05 20:11:44 -0800

    Second round completion with 58% faster than others

--- searchRotatedArray.js
+++ searchRotatedArray.js
@@ -4,35 +4,43 @@
  * @return {number}
  */
 var search = function(nums, target) {
+    if (!nums || nums.length === 0) return -1;
+    if (nums.length === 1) {
+        if (target === nums[0]) return 0;
+        if (target !== nums[0]) return -1;
+    }
+    
     let start = 0;
-    let end = nums.length;
-    let found = false;
-    nums.sort((a,b) => {
-        return a-b
-    })
-    console.log();
-    while(found === false) {
-         var middleIndex = Math.floor((start + end) / 2);
-         console.log(middleIndex);
-         if (start === end && nums[start] === target) {
-             found = true;
-             return start;
-         } else if (start === end && nums[start] !== target) {
-             found = true;
-             return -1;
-         }
-        if (nums[middleIndex] > target) {
-            start = 0;
-            end = middleIndex - 1;
-        } else if (nums[middleIndex] < target) {
-            start = middleIndex + 1;
-            // end = nums.length;
-        } else if (nums[middleIndex] === target) {
-            found = true;
-            return middleIndex;
+    let end = nums.length - 1;
+    
+    while (start < end) {
+        let mid = Math.floor((start + end) / 2);
+        if (nums[mid] < nums[end]) {
+            end = mid;
+        } else {
+            start = mid + 1;
+        }
+    }
+    
+    let minPoint = start;
+    start = 0;
+    end = nums.length - 1;
+    if (target === nums[minPoint]) return minPoint
+    
+    if (target >= nums[minPoint] && target <= nums[end]) {
+        start = minPoint;
+    } else {
+        end = minPoint - 1;
+    }
+
+    while(start <= end) {
+        let mid = Math.floor((start + end) / 2);
+        if (target === nums[mid]) return mid;
+        if (target > nums[mid]) {
+            start = mid + 1;
+        } else {
+            end = mid - 1
         }
     }
     return -1;
 };
-
-console.log(search([1,2,4,5,7,8], 8));
